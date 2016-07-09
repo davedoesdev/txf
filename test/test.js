9614ba232577484179bfbb7ccfc326144cd5798c
David Halls <dahalls@gmail.com>
2016-07-09 08:14:01 +0100

    Add auth tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
 /*
 Test different status codes
-Test no auth and auth
+Test no auth and auth including wrong secret
 Test content-type headers
 Long paths
+Wrong number of path separators
 100% coverage
 Make logging optional somehow?
 */
@@ -16,6 +17,17 @@ var http = require('http'),
 var port = 8700;
 var url = 'http://localhost:' + port + '/';
 
+var secrets = {
+    foo: {
+        sender: '',
+        receiver: ''
+    },
+    prot: {
+        sender: 'foobar',
+        receiver: 'fred bloggs'
+    }
+};
+
 describe('txf', function ()
 {
     var server;
@@ -24,7 +36,7 @@ describe('txf', function ()
     {
         server = http.createServer();
         server.listen(port, cb);
-        txf(server, { foo: { sender: '', receiver: '' } });
+        txf(server, secrets);
     });
 
     after(function (cb)
@@ -32,77 +44,110 @@ describe('txf', function ()
         server.close(cb);
     });
 
-    it('should transfer data', function ()
+    function test(description, make_put_url, make_get_url)
     {
-        var get_response = chakram.get(url + 'foo/bar');
-        expect(get_response).to.have.status(200);
-        expect(get_response).to.have.json('hello');
-
-        var put_response = chakram.put(url + 'foo/bar', 'hello')
-        expect(put_response).to.have.status(200);
-
-        return chakram.wait();
-    });
-
-    it('should support multiple transfers at the same time', function ()
+    describe(description, function ()
     {
-        this.timeout(60 * 1000);
-
-        for (var i = 0; i < 1000; i += 1)
+        it('should transfer data', function ()
         {
-            var get_response = chakram.get(url + 'foo/bar' + i);
+            var get_response = chakram.get(make_get_url('bar'));
             expect(get_response).to.have.status(200);
-            expect(get_response).to.have.json('hello' + i);
+            expect(get_response).to.have.json('hello');
 
-            var put_response = chakram.put(url + 'foo/bar' + i, 'hello' + i);
+            var put_response = chakram.put(make_put_url('bar'), 'hello');
             expect(put_response).to.have.status(200);
-        }
 
-        return chakram.wait();
-    });
-
-    it('should error if get request is in progress', function ()
-    {
-        var get_response = chakram.get(url + 'foo/bar');
-        expect(get_response).to.have.status(200);
-        expect(get_response).to.have.json('hello');
-
-        expect(chakram.get(url + 'foo/bar')).to.have.status(409);
-
-        var put_response = chakram.put(url + 'foo/bar', 'hello')
-        expect(put_response).to.have.status(200);
-
-        return chakram.wait();
-    });
-
-    it('should error if put request is in progress', function ()
-    {
-        var put_response = chakram.put(url + 'foo/bar', 'hello');
-        expect(put_response).to.have.status(200);
-
-        expect(chakram.put(url + 'foo/bar', 'hello')).to.have.status(409);
-
-        var get_response = chakram.get(url + 'foo/bar');
-        expect(get_response).to.have.status(200);
-        expect(get_response).to.have.json('hello');
-
-        return chakram.wait();
-    });
-
-    it.only('should support large transfers', function ()
-    {
-        var buf = crypto.randomBytes(1024 * 1024);
-
-        var put_response = chakram.put(url + 'foo/bar', buf, { json: false });
-        expect(put_response).to.have.status(200);
-
-        chakram.get(url + 'foo/bar', { encoding: null })
-        .then(function (get_response)
-        {
-            expect(get_response).to.have.status(200);
-            return expect(get_response.body.equals(buf)).to.equal(true);
+            return chakram.wait();
         });
 
-        return chakram.wait();
+        it('should support multiple transfers at the same time', function ()
+        {
+            this.timeout(60 * 1000);
+
+            for (var i = 0; i < 1000; i += 1)
+            {
+                var get_response = chakram.get(make_get_url('bar' + i));
+                expect(get_response).to.have.status(200);
+                expect(get_response).to.have.json('hello' + i);
+
+                var put_response = chakram.put(make_put_url('bar' + i), 'hello' + i);
+                expect(put_response).to.have.status(200);
+            }
+
+            return chakram.wait();
+        });
+
+        it('should error if get request is in progress', function ()
+        {
+            var get_response = chakram.get(make_get_url('bar'));
+            expect(get_response).to.have.status(200);
+            expect(get_response).to.have.json('hello');
+
+            expect(chakram.get(make_get_url('bar'))).to.have.status(409);
+
+            var put_response = chakram.put(make_put_url('bar'), 'hello');
+            expect(put_response).to.have.status(200);
+
+            return chakram.wait();
+        });
+
+        it('should error if put request is in progress', function ()
+        {
+            var put_response = chakram.put(make_put_url('bar'), 'hello');
+            expect(put_response).to.have.status(200);
+
+            expect(chakram.put(make_put_url('bar'), 'hello')).to.have.status(409);
+
+            var get_response = chakram.get(make_get_url('bar'));
+            expect(get_response).to.have.status(200);
+            expect(get_response).to.have.json('hello');
+
+            return chakram.wait();
+        });
+
+        it('should support large transfers', function ()
+        {
+            var buf = crypto.randomBytes(1024 * 1024);
+
+            var put_response = chakram.put(make_put_url('bar'), buf, { json: false });
+            expect(put_response).to.have.status(200);
+
+            return chakram.all([
+
+            chakram.get(make_get_url('bar'), { encoding: null })
+            .then(function (get_response)
+            {
+                expect(get_response).to.have.status(200);
+                expect(get_response.body.equals(buf)).to.equal(true);
+            }),
+
+            chakram.wait()]);
+        });
+    });
+    }
+
+    function make_url_no_auth(path, namespace)
+    {
+        return url + (namespace || 'foo') + '/' + path;
+    }
+
+    test('no auth', make_url_no_auth, make_url_no_auth);
+
+    test('auth', function (path, namespace)
+    {
+        return url +
+               (namespace || 'prot') + '/' +
+               crypto.createHmac('sha256', secrets.prot.sender)
+                   .update(path)
+                   .digest('hex') + '/' +
+               path;
+    }, function (path, namespace)
+    {
+        return url +
+               (namespace || 'prot') + '/' +
+               crypto.createHmac('sha256', secrets.prot.receiver)
+                   .update(path)
+                   .digest('hex') + '/' +
+               path;
     });
 });
