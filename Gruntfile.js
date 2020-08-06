3f8aed786ceb8e1d63ff1290101d0c761e3d9af5
David Halls <dahalls@gmail.com>
2020-08-06 11:57:06 +0100

    Update deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,48 +23,30 @@ module.exports = function (grunt)
             extraHeadingLevels: 1
         },
 
-        bgShell: {
-            cover: {
-                cmd: "./node_modules/.bin/nyc -x Gruntfile.js -x 'test/**' ./node_modules/.bin/grunt test",
-                fail: true,
-                execOpts: {
-                    maxBuffer: 0
-                }
-            },
+        exec: {
+            cover: "./node_modules/.bin/nyc -x Gruntfile.js -x 'test/**' ./node_modules/.bin/grunt test",
 
-            cover_report: {
-                cmd: './node_modules/.bin/nyc report -r lcov',
-                fail: true
-            },
+            cover_report: './node_modules/.bin/nyc report -r lcov',
 
-            cover_check: {
-                cmd: './node_modules/.bin/nyc check-coverage --statements 100 --branches 100 --functions 100 --lines 100',
-                fail: true
-            },
+            cover_check: './node_modules/.bin/nyc check-coverage --statements 100 --branches 100 --functions 100 --lines 100',
 
-            coveralls: {
-                cmd: 'cat coverage/lcov.info | coveralls',
-                fail: true
-            },
+            coveralls: 'cat coverage/lcov.info | coveralls',
 
-            certs: {
-                cmd: 'make -C test -f Makefile.certs',
-                fail: true
-            }
+            certs: 'make -C test -f Makefile.certs'
         }
     });
     
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-apidox');
-    grunt.loadNpmTasks('grunt-bg-shell');
+    grunt.loadNpmTasks('grunt-exec');
 
     grunt.registerTask('lint', 'jshint');
-    grunt.registerTask('test', ['bgShell:certs', 'mochaTest']);
+    grunt.registerTask('test', ['exec:certs', 'mochaTest']);
     grunt.registerTask('docs', 'apidox');
-    grunt.registerTask('coverage', ['bgShell:cover',
-                                    'bgShell:cover_report',
-                                    'bgShell:cover_check']);
-    grunt.registerTask('coveralls', 'bgShell:coveralls');
+    grunt.registerTask('coverage', ['exec:cover',
+                                    'exec:cover_report',
+                                    'exec:cover_check']);
+    grunt.registerTask('coveralls', 'exec:coveralls');
     grunt.registerTask('default', ['lint', 'test']);
 };
