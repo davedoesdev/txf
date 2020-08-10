774fdb220de9d8b4f4ce7beea6ed89711b27bccf
David Halls <dahalls@gmail.com>
2020-08-10 14:26:06 +0100

    Add support for Heroku

--- server.js
+++ server.js
@@ -4,16 +4,15 @@ var http = require('http'),
     txf = require('./index.js'),
     server = http.createServer();
 
-var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = process.env.PORT || 8080;
 var default_sender_secret = process.env.DEFAULT_SENDER_SECRET || '';
 var default_receiver_secret = process.env.DEFAULT_RECEIVER_SECRET || '';
 
-console.log('starting', ip, port);
+console.log('starting on port', port);
 
-server.listen(port, ip, function()
+server.listen(port, function()
 {
-    console.log('listening on', ip, port);
+    console.log('listening on port', port);
 
     server.on('request', function (request)
     {
